7429fc3a6993a5847d0e93c93b36f111fa047eb5
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-02-06 10:51:21 +1300

    Add favicon and banner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
 // Components
-import NavBar from './components/NavBar';
-import {
-  AppWrap,
-} from './components/wrapper'
-import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
+import NavBar from './components/NavBar'
+import { AppWrap } from './components/wrapper'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
 
 // Styles
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <>
     <NavBar />
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-            {/* Routes */}
-            <Routes>
-          <Route path="/"
-            element={<Home />} />
-          <Route
-            path="/contact"
-            element={<Contact />}
-          />
-          <Route
-            path="/about"
-            element={<About />}
-          />
-        </Routes>
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+      </Routes>
     </>
-  );
+  )
 }
 
 export default AppWrap(App)
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './NavBar.css'
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import logo from '../assets/logo.svg'
+
 
 
 const NavBar = () => {
 
   return (
     <Navbar expand="lg">
-      <Navbar.Brand as={Link} to="/">CLEAN TECH</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        <img src={logo} width="65" height="65" alt="Logo" /></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
