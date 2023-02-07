91bc847d452a9fe7b30a8ded1c5e721420bdf794
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-02-07 17:39:12 +1300

    Link up contact form to send to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,53 +1,86 @@
-import React from 'react';
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+} from 'react-bootstrap'
 // Icons
 import {
   FaPhone,
   FaEnvelope,
-} from 'react-icons/fa';
+} from 'react-icons/fa'
 
 // Styles
 import './Contact.css'
 
 const Contact = () => {
+  const form = useRef(null)
+  const [showAlert, setShowAlert] =
+    useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    setShowAlert(true)
+    emailjs
+      .sendForm(
+        'service_it4gqsh',
+        'template_kv5t2hu',
+        form.current,
+        'ucKpajsa8GadSBksi'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+    form.current.reset()
+  }
+
   return (
     <footer>
-    <Container>
-      <Row>
-        <Col md={6}>
-          <h5>Contact Us</h5>
-          <p>
-          Fill out the contact form or send a text.
-          </p>
-          <p>
-          We aim to respond within 24 hours.
-          </p>
-          <div className="app__footer-cards">
-          <div className="app__footer-card">
-          <FaPhone />
-          &nbsp;&nbsp;
-            <a
-              href="tel:+64 27 365 8483"
-              className="p-text"
-              rel="noreferrer"
-            >
-              +64 27 365 8483
-            </a>
-          </div>
-          <div className="app__footer-card">
-          <FaEnvelope />
-          &nbsp;&nbsp;
-            <a
-              href="mailto:admin@cleantechmaintenance.co.nz"
-              target="_blank"
-              className="p-text"
-              rel="noreferrer"
-            >
-              admin@cleantechmaintenance.co.nz
-            </a>
-          </div>
-          </div>
-          {/* <h5>Links</h5>
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h5>Contact Us</h5>
+            <p>
+              Fill out the contact form or send a
+              text.
+            </p>
+            <p>
+              We aim to respond within 24 hours.
+            </p>
+            <div className="app__footer-cards">
+              <div className="app__footer-card">
+                <FaPhone />
+                &nbsp;&nbsp;
+                <a
+                  href="tel:+64 27 365 8483"
+                  className="p-text"
+                  rel="noreferrer"
+                >
+                  +64 27 365 8483
+                </a>
+              </div>
+              <div className="app__footer-card">
+                <FaEnvelope />
+                &nbsp;&nbsp;
+                <a
+                  href="mailto:admin@cleantechmaintenance.co.nz"
+                  target="_blank"
+                  className="p-text"
+                  rel="noreferrer"
+                >
+                  admin@cleantechmaintenance.co.nz
+                </a>
+              </div>
+            </div>
+            {/* <h5>Links</h5>
           <ul className="list-unstyled">
             <li>
               <a href="#">Twitter</a>
@@ -59,35 +92,75 @@ const Contact = () => {
               <a href="#">Instagram</a>
             </li>
           </ul> */}
-        </Col>
-        <Col md={6}>
-          <Form>
-          <Form.Group controlId="formBasicName">
-                <Form.Label><h6>Name</h6></Form.Label>
-                <Form.Control type="text" placeholder="Enter your name or company" />
+          </Col>
+          <Col md={6}>
+            <Form ref={form} onSubmit={sendEmail}>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>
+                  <h6>Name</h6>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="from_name"
+                  id="from_name"
+                  placeholder="Enter your name or company"
+                />
               </Form.Group>
               <Form.Group controlId="formBasicPhone">
-                <Form.Label><h6>Phone</h6></Form.Label>
-                <Form.Control type="tel" placeholder="Enter phone number" />
+                <Form.Label>
+                  <h6>Phone</h6>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  for="phone"
+                  name="phone"
+                  id="phone"
+                  placeholder="Enter phone number"
+                />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
-              <Form.Label><h6>Email</h6></Form.Label>
-              <Form.Control type="email" placeholder="Enter email address" />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label><h6>Message</h6></Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-            <Button variant="secondary" type="submit"
-            style = {{margin: '10px'}}>
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-);
+                <Form.Label>
+                  <h6>Email</h6>
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  for="reply_to"
+                  name="reply_to"
+                  id="reply_to"
+                  placeholder="Enter email address"
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>
+                  <h6>Message</h6>
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="message"
+                  rows={3}
+                />
+              </Form.Group>
+              <Button
+                variant="secondary"
+                type="submit"
+                style={{ margin: '10px' }}
+              >
+                Submit
+              </Button>
+            </Form>
+            {showAlert && (
+              <div
+                className="alert alert-success"
+                role="alert"
+              >
+                Message sent!
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Contact
