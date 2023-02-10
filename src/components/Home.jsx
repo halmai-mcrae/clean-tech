67df7cd29c6c04dcee8383d4475c754db210b1ea
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-02-10 16:47:50 +1300

    Add services component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import {
   Container,
   Row,
@@ -10,105 +10,62 @@ import Footer from './Footer'
 
 // Icons
 import { BsShieldFillCheck } from 'react-icons/bs'
-import {
-  IoConstructSharp,
-  IoAddCircle,
-} from 'react-icons/io5'
-import {
-  FaEnvira,
-  FaCheckCircle,
-  FaLayerGroup,
-  FaThumbsUp,
-} from 'react-icons/fa'
+import { IoAddCircle } from 'react-icons/io5'
+import { FaEnvira } from 'react-icons/fa'
 
 const Home = () => {
+  const Services = lazy(() =>
+    import('./Services')
+  )
   return (
     <>
-      <Container className="my-5">
+      <Container className="jumbotron">
         <Row>
           <Col>
-            <div className="jumbotron">
-              <p>
-                Clean Tech Maintenance is a
-                dedicated nationwide provider
-                serving maintenance plans for
-                commercial properties. With years
-                of experience and a highly trained
-                team of professionals, we are
-                equipped to best protect and
-                maintain our customers'
-                investments.
-              </p>
-              <p>
-                We specialise in routine
-                maintenance and maintenance plans
-                with the goal to ensure the
-                longevity and durability of your
-                roof and/or cladding to fulfil
-                and uphold the product warranty.
-              </p>
-              <p>
-                We are a team of highly skilled
-                tradespeople who have a passion
-                for delivering quality workmanship
-                and exceptional customer service.
-              </p>
-              <h4>
-                <BsShieldFillCheck /> Warranty
-                Protection
-              </h4>
-              <p>
-                Your roof and/or cladding is a
-                significant investment, and we
-                want to ensure it is protected.
-                That's why we guarantee strict
-                scheduling to fall within warranty
-                requirements, along with annual
-                reports to ensure that any
-                possible claims are clear of
-                dispute.
-              </p>
-              <h4>
-                <IoAddCircle /> Reliable
-              </h4>
-              <p>
-                When it comes to your maintenance,
-                reliability is key. That's why we
-                are committed to providing prompt
-                and dependable services, no matter
-                the size or complexity of the job.
-                Our team of experts is always
-                available to answer any questions
-                and provide the support you need.
-              </p>
-              <h4>
-                <IoConstructSharp /> Durable
-              </h4>
-              <p>
-                We use only the correct materials
-                as specified to ensure your roof
-                stays within its warranty
-                requirements. These products are
-                designed to help withstand our
-                harsh UV and coastal environments
-                here in New Zealand providing
-                long-lasting protection and peace
-                of mind.
-              </p>
-              <p>
-                Please contact us for more
-                information and feel free to
-                download our data information
-                sheets.
-              </p>            <h4>
-              <FaThumbsUp /> Tested Maintenance
-              Solution
+            <p>
+              Clean Tech Maintenance is a
+              dedicated nationwide provider
+              serving maintenance plans for
+              commercial properties. With years of
+              experience and a highly trained team
+              of professionals, we are equipped to
+              best protect and maintain our
+              customers' investments.
+            </p>
+            <p>
+              We specialise in routine maintenance
+              and maintenance plans with the goal
+              to ensure the longevity and
+              durability of your roof and/or
+              cladding to fulfil and uphold the
+              product warranty.
+            </p>
+            <h4>
+              <BsShieldFillCheck /> Warranty
+              Protection
             </h4>
             <p>
-              Our washes have been tested and
-              assessed as per NZBC C/AS2:2019.
-              Group rating of 2-S on Type 1 or 1-S
-              on Type 3 substrates.
+              Your roof and/or cladding is a
+              significant investment, and we want
+              to ensure it is protected. That's
+              why we guarantee strict scheduling
+              to fall within warranty
+              requirements, along with annual
+              reports to ensure that any possible
+              claims are clear of dispute.
+            </p>
+            <h4>
+              <IoAddCircle /> Reliable
+            </h4>
+            <p>
+              When it comes to your maintenance,
+              reliability is key. That's why we
+              are committed to providing prompt
+              and dependable services, no matter
+              the size or complexity of the job.
+              Our team of experts is always
+              available to answer any questions
+              and provide the support you need.
             </p>
             <h4>
               <FaEnvira /> Eco-Friendly
@@ -119,33 +76,18 @@ const Home = () => {
               – 49 gram) Non-Dangerous Good
               according to NZS 5433.
             </p>
-
-            <h4>
-              <FaCheckCircle /> Ease of
-              Application
-            </h4>
             <p>
-              Our single component cleaners are
-              designed for easy, slow release
-              application that stays effective for
-              up to three months. Whether you're
-              managing a new construction project
-              or need remediation work done, our
-              solutions can serve multiple
-              purposes.
+              Please contact us for more
+              information and feel free to
+              download our data information
+              sheets.
             </p>
-            <h4>
-              <FaLayerGroup /> Multi Substrate
-            </h4>
-            <p>
-              Our washes are suitable for use on
-              stainless steel, zinc, aluminium,
-              solar panels and structural steel.
-            </p>
-            </div>
           </Col>
         </Row>
       </Container>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Services />
+      </Suspense>
       <Footer />
     </>
   )
---
+++ src/components/Services.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import {
+  Container,
+  Image,
+  Col,
+  Row,
+} from 'react-bootstrap'
+
+// Photos
+import cleantech0 from '../assets/cleantech0.png'
+import cleantech1 from '../assets/cleantech1.png'
+import cleantech2 from '../assets/cleantech2.png'
+import cleantech3 from '../assets/cleantech3.png'
+import cleantech4 from '../assets/cleantech4.png'
+
+//icons
+import { GrBusinessService } from 'react-icons/gr'
+import { MdOutlineHealthAndSafety} from 'react-icons/md'
+import { GiSolarPower } from 'react-icons/gi'
+import { SiSeat } from 'react-icons/si'
+
+
+import './Services.css'
+
+const Services = () => {
+  return (
+    <>
+      <Container className='jumbotron'>
+      <h4 className="text-center">
+            <GrBusinessService /> Our Services
+          </h4>
+        <Row>
+          <Col md={6}>
+            <Image
+              src={cleantech0}
+              alt="Power washing"
+              fluid
+              className="image-size"
+            />
+          </Col>
+          <Col md={6}>
+          <h6 className="text-center">
+            <MdOutlineHealthAndSafety /> Health and Safety
+          </h6>
+            <p>
+            Safe accessibility and a stable working platform is a key ingredient to our day to day operations. We have ample experience in the operating of scissor lifts, cherry pickers, extendable ladders and the construction of mobile scaffolding.
+            </p>
+            <p>
+            Health and safety is our number one priority. We maintain a high standard of workplace consciousness and operate a job specific health and safety plan in accordance to the New Zealand Health and Safety at Work Act 2015. We are active members of NZ Site Safe and all our employees are IRATA working at heights qualified.
+            </p>
+          </Col>
+          <Col md={6}>
+          <h6 className="text-center">
+            <GiSolarPower /> Solar Panels
+          </h6>
+            <p>
+            Cleaning underneath rooftop solar panels are a vital part of a maintenance schedule. These areas remain unwashed by natural rain fall and experience large build up around “panel to roof” fixings which can lead to major leaks. This will often be where you can expect failures if not maintained regularly.
+            </p>
+            <p>
+            Solar panel washes require a more sensitive approach. This service we perform by manual soft wash to ensure there is not excessive pressure applied to the panels, this keeping them protected, clean and corrosion free.
+            </p>
+          </Col>
+          <Col md={6}>
+            <Image
+              src={cleantech1}
+              alt="Saftey harness"
+              fluid
+              className="image-size"
+            />
+          </Col>
+          
+          <Col md={6}>
+            <Image
+              src={cleantech3}
+              alt="Solar panels"
+              fluid
+              className="image-size"
+            />
+          </Col>
+          <Col md={6}>
+            <Image
+              src={cleantech4}
+              alt="Solar panels"
+              fluid
+              className="image-size"
+            />
+          </Col>
+          <Col>
+          <h6 className="text-center">
+            <SiSeat /> Stadium Seating
+          </h6>
+            <p>
+            Seating is an essential aspect of any sports or entertainment venue. Providing comfortable and functional seating for spectators is crucial to creating a positive experience for fans. Regular maintenance of stadium seating is important to keep it in good condition and to ensure that fans have a comfortable place to sit during events.            </p>
+            <p>
+            One important aspect of stadium seating maintenance is cleaning. Dirt, debris, and spills can accumulate on seats over time, making them look worn and unattractive. Regular cleaning helps to maintain the appearance of the seats and keep them looking like new.            </p>
+          </Col>
+          <Col md={6}>
+            <Image
+              src={cleantech2}
+              alt="Stadium seating"
+              fluid
+              className="image-size"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Services
