baa5af4c97b426f102dfe7bb83a061ef69809041
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-02-10 16:49:15 +1300

    Fix NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,56 @@
 import React from 'react'
 import './NavBar.css'
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from 'react-router-dom'
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+} from 'react-bootstrap'
 import logo from '../assets/logo.svg'
 import infosheet from '../assets/CleanTechInfoSheet.pdf'
 
-
-
 const NavBar = () => {
-
   return (
     <Navbar expand="lg">
       <Navbar.Brand as={Link} to="/">
-        <img src={logo} width="50" height="50" alt="Logo" /></Navbar.Brand>
+        <img
+          src={logo}
+          width="60"
+          height="60"
+          alt="Logo"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          <NavDropdown title="Information" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+          <NavDropdown
+            title="Information"
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item
+              as={Link}
+              to="/about"
+            >
+              About
+            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href={infosheet} target="_blank">Download PDF Datasheet</NavDropdown.Item>
+            <NavDropdown.Item
+              href={infosheet}
+              target="_blank"
+            >
+              Download PDF Datasheet
+            </NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
