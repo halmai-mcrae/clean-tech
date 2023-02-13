850c6485270a377047eb4a984149834a8cea8875
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-02-13 18:11:03 +1300

    Add packages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ import {
 } from 'react-bootstrap'
 
 // Components
+import Contact from './Contact'
 import Footer from './Footer'
+import Packages from './Packages'
 
 // Icons
-import { BsShieldFillCheck } from 'react-icons/bs'
-import { IoAddCircle } from 'react-icons/io5'
-import { FaEnvira } from 'react-icons/fa'
+import { IoLogoReact } from 'react-icons/io5'
 
 const Home = () => {
   const Services = lazy(() =>
@@ -22,72 +22,30 @@ const Home = () => {
       <Container className="jumbotron">
         <Row>
           <Col>
+            <h4 className="text-center">
+              <IoLogoReact /> Commercial
+              Maintenance & Service Plans
+            </h4>
             <p>
               Clean Tech Maintenance is a
               dedicated nationwide provider
               serving maintenance plans for
-              commercial properties. With years of
-              experience and a highly trained team
-              of professionals, we are equipped to
-              best protect and maintain our
-              customers' investments.
-            </p>
-            <p>
-              We specialise in routine maintenance
-              and maintenance plans with the goal
-              to ensure the longevity and
-              durability of your roof and/or
-              cladding to fulfil and uphold the
-              product warranty.
-            </p>
-            <h4>
-              <BsShieldFillCheck /> Warranty
-              Protection
-            </h4>
-            <p>
-              Your roof and/or cladding is a
-              significant investment, and we want
-              to ensure it is protected. That's
-              why we guarantee strict scheduling
-              to fall within warranty
-              requirements, along with annual
-              reports to ensure that any possible
-              claims are clear of dispute.
-            </p>
-            <h4>
-              <IoAddCircle /> Reliable
-            </h4>
-            <p>
-              When it comes to your maintenance,
-              reliability is key. That's why we
-              are committed to providing prompt
-              and dependable services, no matter
-              the size or complexity of the job.
-              Our team of experts is always
-              available to answer any questions
-              and provide the support you need.
-            </p>
-            <h4>
-              <FaEnvira /> Eco-Friendly
-            </h4>
-            <p>
-              Water-based 100% Asbestos and
-              Halogen free Low VOC (Limits, 5 gram
-              – 49 gram) Non-Dangerous Good
-              according to NZS 5433.
-            </p>
-            <p>
-              Please contact us for more
-              information and feel free to
-              download our data information
-              sheets.
+              commercial properties. We specialise
+              in routine maintenance and
+              maintenance plans with the goal to
+              ensure the longevity and durability
+              of your roof and/or cladding to
+              fulfil and uphold the product
+              warranty.
             </p>
           </Col>
         </Row>
       </Container>
+      <Packages />
       <Suspense fallback={<div>Loading...</div>}>
         <Services />
       </Suspense>
+      <Contact />
       <Footer />
     </>
   )
---
+++ src/components/Packages.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import {
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap'
+
+// Icons
+import { BsCheck } from 'react-icons/bs'
+import { AiFillFilePdf } from 'react-icons/ai'
+
+// Styles
+import './Packages.css'
+import { IoLogoReact } from 'react-icons/io5'
+
+const Packages = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="package-card-0" md={6}>
+            <h6 className="text-center">
+              <IoLogoReact /> New Build
+              Maintenance Plan
+            </h6>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+              et dolore magna aliqua.
+            </p>
+            <ul>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+            </ul>
+            <h6 className="text-center">
+              <a href={''} target="_blank">
+                <AiFillFilePdf /> Download PDF
+                Infosheet.
+              </a>
+            </h6>
+          </Col>
+          <Col className="package-card-1" md={6}>
+            <h6
+              className="text-center"
+              style={{ whiteSpace: 'nowrap' }}
+            >
+              <IoLogoReact /> Existing Structure
+              Maintenance Plan
+            </h6>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+              et dolore magna aliqua.
+            </p>
+            <ul>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+              <li>
+                <BsCheck /> Lorem ipsum dolor sit
+                amet.
+              </li>
+            </ul>
+            <h6 className="text-center">
+              <a href={''} target="_blank">
+                <AiFillFilePdf /> Download PDF
+                Infosheet.
+              </a>
+            </h6>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Packages
